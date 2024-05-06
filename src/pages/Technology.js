508e0b414e32a685de9c1147c609ec1268c43002
Siddharth <56434063+siddharthsbhadauria@users.noreply.github.com>
2024-05-06 18:35:07 +0100

    Test

---
+++ src/pages/Technology.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Technology = () => (
+  <div>
+    <p>Welcome to my technology page. Here, you'll find articles and content about my favorite gadgets, coding projects, and tech trends.</p>
+  </div>
+);
+
+export default Technology;
